6d67d70f62145b440e2c0f355bebda3aa7cc8e7b
Evgen Artemenko <artemenkojenia.ea@gmail.com>
2016-07-19 18:18:43 +0300

    first filling the file PlatformFreightWagon

--- src/ua/com/westwind/project/model/wagons/freightwagons/PlatformFreightWagon.java
+++ src/ua/com/westwind/project/model/wagons/freightwagons/PlatformFreightWagon.java
@@ -1,4 +1,9 @@
 package ua.com.westwind.project.model.wagons.freightwagons;
 
 public class PlatformFreightWagon extends FreightWagon {
+    public static final TypeFreightWagon TYPE_WAGON = TypeFreightWagon.PLATFORM;
+
+    public PlatformFreightWagon() {
+        super(TYPE_WAGON);
+    }
 }

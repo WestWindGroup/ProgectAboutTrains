b71bcca6886db202d25ea2e7d1bcb20d468807a4
Evgen Artemenko <artemenkojenia.ea@gmail.com>
2016-07-19 17:41:11 +0300

    first filling the file CoupeSVWagon

--- src/ua/com/westwind/project/model/wagons/passengerwagons/CoupeSVWagon.java
+++ src/ua/com/westwind/project/model/wagons/passengerwagons/CoupeSVWagon.java
@@ -1,4 +1,10 @@
 package ua.com.westwind.project.model.wagons.passengerwagons;
 
 public class CoupeSVWagon extends PassengerWagon {
+    public static final TypeWagon TYPE_WAGON = TypeWagon.COUPE_SV;
+    private static final int COUNT_PLACE = 20;
+
+    public CoupeSVWagon() {
+        super(TYPE_WAGON, COUNT_PLACE);
+    }
 }

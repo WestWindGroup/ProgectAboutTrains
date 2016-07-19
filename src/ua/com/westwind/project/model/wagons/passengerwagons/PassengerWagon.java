717b9edd5958f5f190e89fb0100b2407fe0a043b
Evgen Artemenko <artemenkojenia.ea@gmail.com>
2016-07-19 17:43:34 +0300

    first filling the file PassengerWagon

--- src/ua/com/westwind/project/model/wagons/passengerwagons/PassengerWagon.java
+++ src/ua/com/westwind/project/model/wagons/passengerwagons/PassengerWagon.java
@@ -5,7 +5,38 @@ import ua.com.westwind.project.model.Wagons;
 import java.util.ArrayList;
 
 public class PassengerWagon implements Wagons {
-    TypeWagon typeWagon;
-    int countPlace;
-    ArrayList<Place>listPlace;
+
+    protected TypeWagon typeWagon;
+    protected int countPlace;
+    protected ArrayList<Place>listPlace;
+
+    public PassengerWagon(TypeWagon typeWagon, int countPlace) {
+        this.typeWagon = typeWagon;
+        this.countPlace = countPlace;
+        this.listPlace = new ArrayList<>(countPlace);
+    }
+
+    public TypeWagon getTypeWagon() {
+        return typeWagon;
+    }
+
+    public void setTypeWagon(TypeWagon typeWagon) {
+        this.typeWagon = typeWagon;
+    }
+
+    public int getCountPlace() {
+        return countPlace;
+    }
+
+    public void setCountPlace(int countPlace) {
+        this.countPlace = countPlace;
+    }
+
+    public ArrayList<Place> getListPlace() {
+        return listPlace;
+    }
+
+    public void setListPlace(ArrayList<Place> listPlace) {
+        this.listPlace = listPlace;
+    }
 }

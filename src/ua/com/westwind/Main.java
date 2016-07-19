cdaeeb2ae3e98f647bbee834a808943d367d61a5
Evgen Artemenko <artemenkojenia.ea@gmail.com>
2016-07-19 16:32:51 +0300

    first filling the file TypeWagon

--- src/ua/com/westwind/Main.java
+++
@@ -1,8 +0,0 @@
-package ua.com.westwind;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- src/ua/com/westwind/project/model/wagons/passengerwagons/TypeWagon.java
+++ src/ua/com/westwind/project/model/wagons/passengerwagons/TypeWagon.java
@@ -1,9 +1,20 @@
 package ua.com.westwind.project.model.wagons.passengerwagons;
 
 public enum TypeWagon {
-    PassengerWaonWithSeating,
-    EconomPassengerWagon,
-    CoupeSV,
-    Coupe,
-    CoupeWagonWithSeating
+    COUPE_SV(0),
+    COUPE(1),
+    ECONOM_PASSENGER_WAGON(2),
+    COUPE_WAGON_WITH_SEATING(3),
+    PASSENGER_WAGON_WITH_SEATING(4);
+
+    private int comfortLevel;
+
+    TypeWagon(int comfortLevel){
+        this.comfortLevel = comfortLevel;
+    }
+
+    public int getComfortLevel() {
+        return comfortLevel;
+    }
+
 }

de4996a43a51efd99a1305ac5c78932b99d09690
Evgen Artemenko <artemenkojenia.ea@gmail.com>
2016-07-19 18:16:28 +0300

    first filling the file IsothermalFreightWagon

--- src/ua/com/westwind/project/model/wagons/freightwagons/IsothermalFreightWagon.java
+++ src/ua/com/westwind/project/model/wagons/freightwagons/IsothermalFreightWagon.java
@@ -1,4 +1,9 @@
 package ua.com.westwind.project.model.wagons.freightwagons;
 
 public class IsothermalFreightWagon extends FreightWagon {
+    public static final TypeFreightWagon TYPE_WAGON = TypeFreightWagon.ISOTHERMAL;
+
+    public IsothermalFreightWagon() {
+        super(TYPE_WAGON);
+    }
 }

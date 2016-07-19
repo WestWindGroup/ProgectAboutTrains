e5f5eb1e328a49f2fcfc68c006abc6b0ab8b26ff
Evgen Artemenko <artemenkojenia.ea@gmail.com>
2016-07-19 18:15:21 +0300

    first filling the file CisternFreightWagon

--- src/ua/com/westwind/project/model/wagons/freightwagons/CisternFreightWagon.java
+++ src/ua/com/westwind/project/model/wagons/freightwagons/CisternFreightWagon.java
@@ -1,4 +1,9 @@
 package ua.com.westwind.project.model.wagons.freightwagons;
 
 public class CisternFreightWagon extends FreightWagon {
+    public static final TypeFreightWagon TYPE_WAGON = TypeFreightWagon.CISTERN;
+
+    public CisternFreightWagon() {
+        super(TYPE_WAGON);
+    }
 }

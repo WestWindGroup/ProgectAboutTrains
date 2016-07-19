eb5476382512b30fa51ac33c70ff3af190f641a6
Evgen Artemenko <artemenkojenia.ea@gmail.com>
2016-07-19 17:43:05 +0300

    first filling the file EconomPassengerWagon

--- src/ua/com/westwind/project/model/wagons/passengerwagons/EconomPassengerWagon.java
+++ src/ua/com/westwind/project/model/wagons/passengerwagons/EconomPassengerWagon.java
@@ -1,4 +1,10 @@
 package ua.com.westwind.project.model.wagons.passengerwagons;
 
 public class EconomPassengerWagon extends PassengerWagon {
+    public static final TypeWagon TYPE_WAGON = TypeWagon.ECONOM_PASSENGER_WAGON;
+    private static final int COUNT_PLACE = 54;
+
+    public EconomPassengerWagon() {
+        super(TYPE_WAGON, COUNT_PLACE);
+    }
 }

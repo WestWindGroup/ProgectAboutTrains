68583aadd7991349348e3b6c99278c612ac62762
Evgen Artemenko <artemenkojenia.ea@gmail.com>
2016-07-19 17:44:08 +0300

    first filling the file PassengerWagonWithSeating

--- src/ua/com/westwind/project/model/wagons/passengerwagons/PassengerWagonWithSeating.java
+++ src/ua/com/westwind/project/model/wagons/passengerwagons/PassengerWagonWithSeating.java
@@ -1,4 +1,13 @@
 package ua.com.westwind.project.model.wagons.passengerwagons;
 
+import java.util.ArrayList;
+
 public class PassengerWagonWithSeating extends PassengerWagon {
+
+    public static final TypeWagon TYPE_WAGON = TypeWagon.PASSENGER_WAGON_WITH_SEATING;
+    private static final int COUNT_PLACE = 60;
+
+    public PassengerWagonWithSeating() {
+        super(TYPE_WAGON, COUNT_PLACE);
+    }
 }

00d89cfdfeba1ed083393a076d397ef1f0a12ee6
Evgen Artemenko <artemenkojenia.ea@gmail.com>
2016-07-19 18:18:18 +0300

    first filling the file CoverredFreightWagon

--- src/ua/com/westwind/project/model/wagons/freightwagons/CoverredFreightWagon.java
+++ src/ua/com/westwind/project/model/wagons/freightwagons/CoverredFreightWagon.java
@@ -1,4 +1,9 @@
 package ua.com.westwind.project.model.wagons.freightwagons;
 
 public class CoverredFreightWagon extends FreightWagon {
+    public static final TypeFreightWagon TYPE_WAGON = TypeFreightWagon.COVERRED;
+
+    public CoverredFreightWagon() {
+        super(TYPE_WAGON);
+    }
 }

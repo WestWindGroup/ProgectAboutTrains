d2b0094f725c1e78a84d4e0ff6280262e6ed5672
Evgen Artemenko <artemenkojenia.ea@gmail.com>
2016-07-19 16:42:23 +0300

    first filling the file Place

--- src/ua/com/westwind/project/model/wagons/passengerwagons/Place.java
+++ src/ua/com/westwind/project/model/wagons/passengerwagons/Place.java
@@ -1,6 +1,29 @@
 package ua.com.westwind.project.model.wagons.passengerwagons;
 
 public class Place {
-    int numberPlace;
-    TypePlace typePlace;
+    private int numberPlace;
+    private TypePlace typePlace;
+
+    public Place(int numberPlace, TypePlace typePlace) {
+        this.numberPlace = numberPlace;
+        this.typePlace = typePlace;
+    }
+
+    public TypePlace getTypePlace() {
+        return typePlace;
+    }
+
+    public void setTypePlace(TypePlace typePlace) {
+        this.typePlace = typePlace;
+    }
+
+    public int getNumberPlace() {
+        return numberPlace;
+    }
+
+    public void setNumberPlace(int numberPlace) {
+        this.numberPlace = numberPlace;
+    }
+
+
 }

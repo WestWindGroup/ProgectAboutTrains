f2362d05faddbe4b2fe6dc97f59255c9f8a9848b
Evgen Artemenko <artemenkojenia.ea@gmail.com>
2016-07-19 17:39:43 +0300

    first filling the file CoupeNormalWagon

--- src/ua/com/westwind/project/model/wagons/passengerwagons/CoupeNormalWagon.java
+++ src/ua/com/westwind/project/model/wagons/passengerwagons/CoupeNormalWagon.java
@@ -1,4 +1,11 @@
 package ua.com.westwind.project.model.wagons.passengerwagons;
 
 public class CoupeNormalWagon extends PassengerWagon {
+
+    public static final TypeWagon TYPE_WAGON = TypeWagon.COUPE;
+    private static final int COUNT_PLACE = 40;
+
+    public CoupeNormalWagon() {
+        super(TYPE_WAGON, COUNT_PLACE);
+    }
 }

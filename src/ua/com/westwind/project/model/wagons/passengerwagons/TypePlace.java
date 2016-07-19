2aa564e0595a6e56513c43991de084e4e92d852f
Evgen Artemenko <artemenkojenia.ea@gmail.com>
2016-07-19 16:37:48 +0300

    first filling the file TypePlace

--- src/ua/com/westwind/project/model/wagons/passengerwagons/TypePlace.java
+++ src/ua/com/westwind/project/model/wagons/passengerwagons/TypePlace.java
@@ -1,8 +1,18 @@
 package ua.com.westwind.project.model.wagons.passengerwagons;
 
 public enum TypePlace {
-    SEAT_PLACE,
-    DOWN_PLACE,
-    UP_PLACE,
-    SIDE_PLACE
+    SEAT_PLACE(0),
+    DOWN_PLACE(1),
+    UP_PLACE(2),
+    SIDE_PLACE(3);
+
+    private int featuresPlace;
+
+    TypePlace(int featuresPlace){
+        this.featuresPlace = featuresPlace;
+    }
+
+    public int getFeaturesPlace() {
+        return featuresPlace;
+    }
 }
